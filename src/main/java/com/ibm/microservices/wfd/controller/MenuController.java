bbf8b890da51e5590ca991c307418f690123691c
Rick Osowski <(none)>
2016-10-18 15:27:00 -0500

    Initial commit for Hystrix support in WFD UI component
    Signed-off-by: Rick Osowski <(none)>

--- src/main/java/com/ibm/microservices/wfd/controller/MenuController.java
+++ src/main/java/com/ibm/microservices/wfd/controller/MenuController.java
@@ -13,6 +13,9 @@ import org.springframework.boot.json.BasicJsonParser;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+
 @Controller
 public class MenuController {
 
@@ -20,29 +23,39 @@ public class MenuController {
   private RestTemplate restTemplate;
 
   @RequestMapping("/")
+  @HystrixCommand(fallbackMethod="getDefaultMenu", groupKey="WhatsForDinnerUI", commandKey="GetMenuForUI")
   public String getMenuForIndex(Model model){
 
-    String menuString = getMenu();
+    String menuString = this.restTemplate.getForObject("http://menu-service/menu", String.class);
+    System.out.println(menuString);
 
-	BasicJsonParser jsonParser = new BasicJsonParser();
-	Map<String, Object> jsonMap = jsonParser.parseMap(menuString);
+    BasicJsonParser jsonParser = new BasicJsonParser();
+    Map<String, Object> jsonMap = jsonParser.parseMap(menuString);
 
-	List<String> appetizerOptions = (List<String>)((Map<String, Object>)jsonMap.get("appetizers")).get("menu");
-	model.addAttribute("appetizerOptions", appetizerOptions);
-	
-	List<String> entreeOptions = (List<String>)((Map<String, Object>)jsonMap.get("entrees")).get("menu");
-	model.addAttribute("entreeOptions", entreeOptions);
-	
-	List<String> dessertOptions = (List<String>)((Map<String, Object>)jsonMap.get("desserts")).get("menu");
-	model.addAttribute("dessertOptions", dessertOptions);
-  
+    List<String> appetizerOptions = (List<String>)((Map<String, Object>)jsonMap.get("appetizers")).get("menu");
+    model.addAttribute("appetizerOptions", appetizerOptions);
+
+    List<String> entreeOptions = (List<String>)((Map<String, Object>)jsonMap.get("entrees")).get("menu");
+    model.addAttribute("entreeOptions", entreeOptions);
+
+    List<String> dessertOptions = (List<String>)((Map<String, Object>)jsonMap.get("desserts")).get("menu");
+    model.addAttribute("dessertOptions", dessertOptions);
+
     return "index";
   }
 
-  private String getMenu() {
-    String menuString = this.restTemplate.getForObject("http://menu-service/menu", String.class);
-    System.out.println(menuString);
-    return menuString;
+  public String getDefaultMenu(Model model){
+
+    List<String> default1 = new ArrayList<String>();
+    model.addAttribute("appetizerOptions", default1);
+
+    List<String> default2 = new ArrayList<String>();
+    model.addAttribute("entreeOptions", default2);
+
+    List<String> default3 = new ArrayList<String>();
+    model.addAttribute("dessertOptions", default3);
+
+    return "index";
   }
 
 }
