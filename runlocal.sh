e08103e877e067e3e0e81d1f25141784bd502c2b
Rick O <rosowski@gmail.com>
2016-11-11 09:57:34 -0600

    Delete runlocal.sh
    Removing extraneous local build files in favor of orchestration tooling and scripting in main project.

--- runlocal.sh
+++
@@ -1,2 +0,0 @@
-echo "Running menu ui, listening on port 8181"
-docker run --name wfd-ui -p 8181:8181 -p 9181:9181 --env-file ~/ibm-cloud-architecture/refarch-cloudnative-container-utils/env/env-eureka-only-local -d wfd-ui:latest
